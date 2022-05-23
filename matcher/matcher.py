e6ff7d014bc130b4ca0b3edcba419df680ca4e59
mohammad Hosein Azaddel <53640254+mohammadhoseinazaddel@users.noreply.github.com>
2022-05-23 15:36:07 +0430

    Update matcher.py

--- matcher/matcher.py
+++ matcher/matcher.py
@@ -44,6 +44,26 @@ def matcher(input_text, patterns=None, splitter=None, end_string=""):
             per_res = {}
     return result
 
+def storcli_matcher(all_disk_attributes_list, wanted_attributes_list):
+    """
+
+    Args:
+        all_disk_attributes_list: contains all disks' information
+        wanted_attributes_list: the keys that wanted to extract from all_disk_attributes_list
+
+    Returns:
+        list of extracted values each element of this list contain extracted features that wanted
+
+    """
+    all_disks_data = []
+    for che_attr in all_disk_attributes_list:
+        found_attrs = {}
+        for attr in wanted_attributes_list:
+            found_attrs[attr] = che_attr[attr]
+        all_disks_data.append(copy.deepcopy(found_attrs))
+        found_attrs.clear()
+    return all_disks_data
+
 
 def convert_tuple_to_pattern_dict(pattern):
     """Converts a list of tuple to a list of dicts. output dicts has a format like
@@ -107,3 +127,97 @@ parse_ret = matcher(prc_cout,
                         ]
                     ), ":",
                     "Drive has flagged a S.M.A.R.T alert")
+
+
+prc_storcli_out = {
+"Controllers":[
+{
+	"Command Status" : {
+		"CLI Version" : "007.0606.0000.0000 Mar 20, 2018",
+		"Operating system" : "Linux 5.4.52-HPDS",
+		"Controller" : 0,
+		"Status" : "Success",
+		"Description" : "Show Drive Information Succeeded."
+	},
+	"Response Data" : {
+		"Drive /c0/e12/s0" : [
+			{
+				"EID:Slt" : "12:0",
+				"DID" : 13,
+				"State" : "Onln",
+				"DG" : 0,
+				"Size" : "237.968 GB",
+				"Intf" : "SATA",
+				"Med" : "SSD",
+				"SED" : "N",
+				"PI" : "N",
+				"SeSz" : "512B",
+				"Model" : "Samsung SSD 860 PRO 256GB",
+				"Sp" : "U",
+				"Type" : "-"
+			}
+		],
+		"Drive /c0/e12/s0 - Detailed Information" :   {
+			"Drive /c0/e12/s0 State" : {
+				"Shield Counter" : 0,
+				"Media Error Count" : 0,
+				"Other Error Count" : 0,
+				"Drive Temperature" : " 21C (69.80 F)",
+				"Predictive Failure Count" : 0,
+				"S.M.A.R.T alert flagged by drive" : "No"
+			},
+			"Drive /c0/e12/s0 Device attributes" : {
+				"SN" : "S42VNF0K516979R",
+				"Manufacturer Id" : "ATA     ",
+				"Model Number" : "Samsung SSD 860 PRO 256GB",
+				"NAND Vendor" : "NA",
+				"WWN" : "5002538e403e1157",
+				"Firmware Revision" : "RVM01B6Q",
+				"Raw size" : "238.474 GB [0x1dcf32b0 Sectors]",
+				"Coerced size" : "237.968 GB [0x1dbf0000 Sectors]",
+				"Non Coerced size" : "237.974 GB [0x1dbf32b0 Sectors]",
+				"Device Speed" : "6.0Gb/s",
+				"Link Speed" : "12.0Gb/s",
+				"NCQ setting" : "Enabled",
+				"Write Cache" : "N/A",
+				"Logical Sector Size" : "512B",
+				"Physical Sector Size" : "512B",
+				"Connector Name" : "Port 0 - 3 "
+			},
+			"Drive /c0/e12/s0 Policies/Settings" : {
+				"Drive position" : "DriveGroup:0, Span:0, Row:0",
+				"Enclosure position" : "1",
+				"Connected Port Number" : "0(path0) ",
+				"Sequence Number" : 2,
+				"Commissioned Spare" : "No",
+				"Emergency Spare" : "No",
+				"Last Predictive Failure Event Sequence Number" : 0,
+				"Successful diagnostics completion on" : "N/A",
+				"SED Capable" : "No",
+				"SED Enabled" : "No",
+				"Secured" : "No",
+				"Cryptographic Erase Capable" : "No",
+				"Locked" : "No",
+				"Needs EKM Attention" : "No",
+				"PI Eligible" : "No",
+				"Certified" : "No",
+				"Wide Port Capable" : "No",
+				"Port Information" : [
+					{
+						"Port" : 0,
+						"Status" : "Active",
+						"Linkspeed" : "12.0Gb/s",
+						"SAS address" : "0x5003048020ca19c0"
+					}
+				]
+			},
+			"Inquiry Data" : "40 00 ff 3f 37 c8 10 00 00 00 00 00 3f 00 00 00 00 00 00 00 34 53 56 32 46 4e 4b 30 31 35 39 36 39 37 20 52 20 20 20 20 00 00 00 00 00 00 56 52 30 4d 42 31 51 36 61 53 73 6d 6e 75 20 67 53 53 20 44 36 38 20 30 52 50 20 4f 35 32 47 36 20 42 20 20 20 20 20 20 20 20 20 20 20 20 20 20 01 80 01 40 00 2f 00 40 00 02 00 02 07 00 ff 3f 10 00 3f 00 10 fc fb 00 01 01 ff ff ff 0f 00 00 07 00 "
+		}
+        
+        
+ parse_ret = helper.storcli_matcher(prc_storcli_out,
+                                           ["Enclosure Device ID", "Slot Number", "Enclosure position", "DID",
+                                            "Intf", "Raw size", "Firmware Revision", "SN", "DG",
+                                            "Link Speed", "Med", "WWN"]
+                                           )
+ 
