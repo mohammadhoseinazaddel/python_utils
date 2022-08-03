f3296a57de6e98ec59b21e61f045c6a294b9bf87
mohammad Hosein Azaddel <53640254+mohammadhoseinazaddel@users.noreply.github.com>
2022-08-03 14:45:28 +0430

    Create groupby.py

---
+++ itertools/groupby.py
@@ -0,0 +1,20 @@
+from itertools import groupby
+
+data = [
+    {"device_id": 1, "port_id": 1, "some_value": 1},
+    {"device_id": 1, "port_id": 2, "some_value": 2},
+    {"device_id": 1, "port_id": 2, "some_value": 3},
+    {"device_id": 2, "port_id": 1, "some_value": 1},
+    {"device_id": 2, "port_id": 1, "some_value": 2},
+    {"device_id": 3, "port_id": 1, "some_value": 1},
+]
+
+grouped_data = groupby(data, key=lambda x: (x["device_id"], x["port_id"]))
+
+for (device_id, port_id), group in grouped_data:
+    print(f"device_id, port_id: {device_id}, {port_id}")
+    print(f"  -> group: {list(group)}")
+    
+grouped_data2 = groupby(data, key=lambda x: x["device_id"])
+for d, group in grouped_data2:
+    print(list(group))
